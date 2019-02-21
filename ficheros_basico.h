069958efcf329ba23471de616d6691546c74e7b8
Alvaro Pons <varopons@gmail.com>
2019-02-21 20:17:19 +0100

    nivel 2 menos el último del parce

--- ficheros_basico.h
+++ ficheros_basico.h
@@ -5,13 +5,27 @@
 #include <unistd.h> // SEEK_SET, read(), write(), open(), close(), lseek()
 #include <errno.h>  //errno
 #include <string.h> // strerror()
+#include "bloques.h"
 
 #define BLOCKSIZE 1024 // bytes
 #define INODOSIZE 128  // bytes
+#define posSB 0        //el superbloque se escribe en el primer bloque de nuestro FS
 
-/*
-    Definición de estructura superbloque
-*/
+struct superbloque{
+	unsigned int posPrimerBloqueMB; //Posición del primer bloque del mapa de bits 
+	unsigned int posUltimoBloqueMB; //Posición del último bloque del mapa de bits 
+	unsigned int posPrimerBloqueAI; //Posición del primer bloque del array de inodos 
+	unsigned int posUltimoBloqueAI; //Posición del último bloque del array de inodos 
+	unsigned int posPrimerBloqueDatos; //Posición del primer bloque de datos 
+	unsigned int posUltimoBloqueDatos; //Posición del último bloque de datos 
+	unsigned int posInodoRaiz; //Posición del inodo del directorio raíz 
+	unsigned int posPrimerInodoLibre; //Posición del primer inodo libre 
+	unsigned int cantBloquesLibres; //Cantidad de bloques libres
+	unsigned int cantInodosLibres; //Cantidad de inodos libres 
+	unsigned int totBloques; //Cantidad total de bloques 
+	unsigned int totInodos; //Cantidad total de inodos 
+	char padding[BLOCKSIZE-12*sizeof(unsigned int)]; //Relleno
+};
 //aqui plis
 
 /*
