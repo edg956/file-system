44c80383d2eaf9666d43a612fc8f3943942b6223
Eugenio Doñaque <eugeniodonaque@gmail.com>
2019-02-21 18:48:14 +0100

    Creados ficheros donde trabajar y comentarios comenzados.

---
+++ ficheros_basico.c
@@ -0,0 +1,23 @@
+#include "ficheros_basico.h"
+
+/*
+    Descripción:
+        Función que calcula el número de bloques necesarios para el mapa de
+        bits.
+        Lo hace mediante la fórmula: (Número de bloques / 8 bits) / BLOCKSIZE.
+
+    Funciones a las que llama:
+
+    Funciones desde donde es llamado:
+
+    Parámetros de entrada:
+        + nbloques: El número de bloques que tiene el sistema de ficheros
+    
+    Parámetros de salida:
+
+*/
+int tamMB(unsigned int nbloques) {}
+int tamAI(unsigned int ninodos) {}
+int initSB(unsigned int nbloques, unsigned int ninodos) {}
+int initMB() {}
+int initAI() {}
