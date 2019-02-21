c04442bf4c98ac108ac2483024298c91032c738d
Eugenio Doñaque <eugeniodonaque@gmail.com>
2019-02-21 18:48:14 +0100

    Creados ficheros donde trabajar y comentarios comenzados.

---
+++ ficheros_basico.h
@@ -0,0 +1,15 @@
+#include <stdio.h>  //printf(), fprintf(), stderr, stdout, stdin
+#include <fcntl.h> //O_WRONLY, O_CREAT, O_TRUNC
+#include <sys/stat.h> //S_IRUSR, S_IWUSR
+#include <stdlib.h>  //exit(), EXIT_SUCCESS, EXIT_FAILURE, atoi()
+#include <unistd.h> // SEEK_SET, read(), write(), open(), close(), lseek()
+#include <errno.h>  //errno
+#include <string.h> // strerror()
+
+#define BLOCKSIZE 1024 // bytes
+
+int tamMB(unsigned int nbloques);
+int tamAI(unsigned int ninodos);
+int initSB(unsigned int nbloques, unsigned int ninodos);
+int initMB();
+int initAI();
