e2d5c48fc628920c584bcfacdf44fe32dddf98da
Eugenio Doñaque <eugeniodonaque@gmail.com>
2019-02-21 19:29:17 +0100

    Agregados comentarios útiles a las funciones y ficheros

--- ficheros_basico.c
+++ ficheros_basico.c
@@ -1,23 +1,97 @@
 #include "ficheros_basico.h"
 
+
 /*
     Descripción:
         Función que calcula el número de bloques necesarios para el mapa de
         bits.
         Lo hace mediante la fórmula: (Número de bloques / 8 bits) / BLOCKSIZE.
 
-    Funciones a las que llama:
-
     Funciones desde donde es llamado:
+        + mi_mkfs.c - main()
 
     Parámetros de entrada:
         + nbloques: El número de bloques que tiene el sistema de ficheros
     
     Parámetros de salida:
-
+        + Número de bloques necesario para el mapa de bits.
 */
 int tamMB(unsigned int nbloques) {}
+
+/*
+    Descripción:
+        Función que calcula el número de inodos en bloques que tendrá el
+        sistema de ficheros.
+        Lo hace mediante la fórmula: (Número de inodos / INODOSIZE) / BLOCKSIZE
+
+    Funciones desde donde es llamado:
+        + mi_mkfs.c - main()
+
+    Parámetros de entrada:
+        + ninodos: El número de inodos que tiene el sistema de ficheros.
+    
+    Parámetros de salida:
+        + Número de bloques necesarios para el número de inodos del sistema.
+*/
 int tamAI(unsigned int ninodos) {}
+
+/*
+    Descripción:
+        Función que inicializa los metadados contenidos en el superbloque a
+        partir de las funciones tamMB() y tamAI(). 
+        Posteriormente, escribe el superbloque inicializado en el bloque posSB
+        del sistema de ficheros.
+    
+    Funciones a las que llama:
+        + bloques.h - bwrite()
+    
+    Funciones desde donde es llamado:
+        + mi_mkfs.c - main()
+
+    Parámetros de entrada:
+        + nbloques: El número de bloques que tiene el sistema de ficheros.
+        + ninodos: El número de inodos que tiene el sistema de ficheros.
+
+    Parámetros de salida:
+        + 0: En ejecución correcta.
+        + (-1): En caso contrario.
+*/
 int initSB(unsigned int nbloques, unsigned int ninodos) {}
+
+/*
+    Descripción:
+        Función que inicializa cada bit del mapa de bits del sistema de ficheros
+        y escribe, byte a byte, cada sección inicializada del MB en el espacio de
+        memoria, dentro del bloque correspondiente, reservado para ello.
+
+    Funciones a las que llama:
+        + bloques.h - bwrite()
+        + bloques.h - bread()
+    
+    Funciones desde donde es llamado:
+        + mi_mkfs.c - main()
+
+    Parámetros de salida:
+        + 0: En ejecución correcta.
+        + (-1): En caso contrario.
+*/
 int initMB() {}
+
+/*
+    Descripción:
+        Función que inicializa el array de inodos y los entrelaza uno a uno.
+        Escribe a partir del bloque del array de inodos del sistema de ficheros
+        el array inicializado.
+    
+    Funciones a las que llama:
+        + bloques.h - bwrite()
+        + bloques.h - bread()
+
+    Funciones desde donde es llamado:
+        + mi_mkfs.c - main()
+
+    Parámetros de salida:
+        + 0: En ejecución correcta,
+        + (-1): En caso contrario.
+*/
 int initAI() {}
