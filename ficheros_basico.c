c7291fd64cebd9785d0618ff181c012aedfb5503
Nadal <nadal12@hotmail.com>
2019-03-21 17:51:10 +0100

    Se añaden las cabeceras de las funciones del nivel 6

--- ficheros_basico.c
+++ ficheros_basico.c
@@ -1098,3 +1098,21 @@ int liberar_bloques_inodo(unsigned int ninodo, unsigned int nblogico){
     return liberados;
     
 }
+
+/*----------------------------FUNCIONES DE NIVEL 6----------------------------*/
+
+int mi_write_f(unsigned int ninodo, const void *buf_original, unsigned int offset, unsigned int nbytes) {
+
+}
+int mi_read_f(unsigned int ninodo, void *buf_original, unsigned int offset, unsigned int nbytes){
+
+}
+int mi_stat_f(unsigned int ninodo, struct STAT *p_stat){
+
+}
+int mi_chmod_f(unsigned int ninodo, unsigned char permisos){
+
+}
+int mi_truncar_f(unsigned int ninodo, unsigned int nbytes){
+    
+}
