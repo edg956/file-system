bf7904fa3f6c64eff9fa5760bd4e7a35d155d46d
nadal <nadal12@hotmail.com>
2019-03-26 12:16:08 +0100

    Se terminan de implementar las funciones 3, 4 y 5 del Nivel 6. Aun hay que revisar las funciones 1 y 2

--- ficheros.h
+++ ficheros.h
@@ -8,3 +8,17 @@ int mi_read_f(unsigned int ninodo, void *buf_original, unsigned int offset, unsi
 int mi_stat_f(unsigned int ninodo, struct STAT *p_stat);
 int mi_chmod_f(unsigned int ninodo, unsigned char permisos);
 int mi_truncar_f(unsigned int ninodo, unsigned int nbytes);
+
+/*Estructura STAT. Igual que la estructura inodo pero sin los punteros.*/
+struct STAT {
+   char tipo;     // Tipo ('l':libre, 'd':directorio o 'f':fichero)
+   char permisos; // Permisos (lectura y/o escritura y/o ejecución)
+   char reservado_alineacion1[6];
+   time_t atime; // Fecha y hora del último acceso a datos: atime
+   time_t mtime; // Fecha y hora de la última modificación de datos: mtime
+   time_t ctime; // Fecha y hora de la última modificación del inodo: ctime
+   unsigned int nlinks;             // Cantidad de enlaces de entradas en directorio
+   unsigned int tamEnBytesLog;      // Tamaño en bytes lógicos
+   unsigned int numBloquesOcupados; // Cantidad de bloques ocupados zona de datos
+   char padding[INODOSIZE - 2 * sizeof(unsigned char) - 3 * sizeof(time_t) - 18 * sizeof(unsigned int) - 6 * sizeof(unsigned char)];
+};
