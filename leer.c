b9c25fefcda2e483e31947cc92f76f5125fdf3f7
nadal <nadal12@hotmail.com>
2019-03-27 12:34:08 +0100

    Se crean los archivos necesarios y se añaden descripciones

---
+++ leer.c
@@ -0,0 +1,9 @@
+/*Le pasaremos por línea de comandos un nº de inodo obtenido con el programa 
+anterior (además del nombre del dispositivo). Su funcionamiento tiene que ser 
+similar a la función cat de linux, explorando TODO el fichero*/
+
+int main(int argc, char **argv) {
+
+
+
+}
