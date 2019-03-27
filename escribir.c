d6c1d99c050b41948cbf346e0f9545dd291b12df
nadal <nadal12@hotmail.com>
2019-03-27 12:34:08 +0100

    Se crean los archivos necesarios y se añaden descripciones

---
+++ escribir.c
@@ -0,0 +1,9 @@
+/* Escribirá texto en uno o varios inodos haciendo uso de reservar_inodo ('f', 6)
+para obtener un nº de inodo, que mostraremos por pantalla y además utilizaremos 
+como parámetro para mi_write_f().*/
+
+int main(int argc, char **argv) {
+
+    
+
+}
