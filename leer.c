b170869cc01767bf0d5c924245cd3f0ccc0a5235
Eugenio <eugeniodonaque@gmail.com>
2019-03-31 20:40:22 +0200

    leer.c implementado

--- leer.c
+++ leer.c
@@ -6,6 +6,9 @@ similar a la función cat de linux, explorando TODO el fichero*/
 #include "ficheros_basico.h"
 #include "ficheros.h"
 #include "bloques.h"
+#include <stdio.h>
+
+#define BUF_SIZE BLOCKSIZE
 
 int main(int argc, char **argv) {
 
@@ -23,6 +26,47 @@ int main(int argc, char **argv) {
         exit(-1);
     }
     
+    int ninodo = atoi(&argv[2]);
+    int bytes_leidos = 0;
+    int aux, offset = 0;
+    unsigned char buffer[BUF_SIZE];
+    struct STAT STAT;
+
+    //Inicializar buffer con memset
+    memset(&buffer, 0, BUF_SIZE);
+
+    printf("Lectura de inodo %i:\n",ninodo);
+
+    //Primera lectura de fichero
+    aux = mi_read_f(ninodo, &buffer, offset, BUF_SIZE);
+
+    //Recorrido al contenido del inodo
+    while (aux > 0) {
+        //Imprimir por pantalla el contenido leído
+        write(1, &buffer, BUF_SIZE);
+        //Reset buffer para próxima lectura
+        memset(&buffer, 0, BUF_SIZE);
+        //Aumentar Nº de bytes leidos
+        bytes_leidos += aux;
+        //Siguiente lectura de fichero
+        offset += BUF_SIZE;
+        aux = mi_read_f(ninodo, &buffer, offset, BUF_SIZE);
+    }
+
+    //Variables para imprimir mensajes sobre bytes leidos a través de stdout
+    char str[256];
+    
+    //Lectura del inodo
+    if (mi_stat_f(ninodo, &STAT) < 0) {
+        perror("Error: no se ha podido obtener información del inodo."
+        "Función -> leer.c - main()");
+        exit(-1);
+    }
+
+    sprintf(&str, "Fin de fichero\n. Nº bytes leidos: %i\nTam.Bytes "
+    "inodo: %i\n",bytes_leidos, STAT.tamEnBytesLog);
+    write(2, &str, strlen(str));
+
     //Desmontar dispositivo virtual. 
     if (bumount()==-1) {
         perror("Error: no se ha podido desmontar el dispositivo virtual.\n");
