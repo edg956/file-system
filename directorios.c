249a7fb2aeebe951a3f27ee82f4096e44c2d83b9
Alvaro Pons <varopons@gmail.com>
2019-04-11 16:14:14 +0000

    extraer_camino done!

--- directorios.c
+++ directorios.c
@@ -1,3 +1,4 @@
+#include <string.h>
 /*
     Descripción:
         Dada una cadena de caracteres camino (que comience por '/'), 
@@ -23,7 +24,33 @@
     Parámetros de salida:
 */
 int extraer_camino(const char *camino, char *inicial, char *final, char *tipo) {
+    if (camino[0] != '/'){
+        return -1;              //Devuelve -1 en caso de que el camino no comience por el carácter '/'
+    }
+    int i = 1;
+    /*
+    En el siguiente bucle guardaremos en inicial la porción comprendida entre los dos primeros '/' o en el caso
+    que no haya un segundo '/' guarda camino en inicial también.
+    */
+    while ((camino[i] != '/') && (camino[i] != '\0')){  
+        inicial[i-1] = camino[i];
+        i++;
+    }
+    
+    if (i == strlen(camino)){
+        final = "\0";
+        tipo = "f";
+        return 0;               //Devolvemos 0 por que lo que hay en el camino corresponde a un fichero
+    }
 
+    int j = 0;
+    while (i < strlen(camino)){
+        final[j] = camino[i];
+        i++;
+        j++;
+    }
+    tipo = "d";
+    return 1;                   //Devolvemos 1 por quelo que hay en el camino corresponde a un directorio
 }
 
 
