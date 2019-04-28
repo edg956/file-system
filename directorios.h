ed1c1003a446c131a663e56720861fc92f498188
Eugenio <eugeniodonaque@gmail.com>
2019-04-28 22:12:28 +0200

    Implementadas funciones casi totalmente. Creados ficheros de programas de nivel 9 y agregados al makefile.

--- directorios.h
+++ directorios.h
@@ -1,6 +1,5 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include "ficheros.h"
-#include "ficheros_basico.h"
 
 /*FUNCIONES NIVEL 8*/
 int extraer_camino(const char *camino, char *inicial, char *final, char *tipo);
@@ -12,8 +11,8 @@ int mi_dir(const char *camino, char *buffer);
 int mi_chmod(const char *camino, unsigned char permisos);
 int mi_stat(const char *camino, struct STAT *p_stat);
 
-//Estructura entrada. 
+/*ESTRUCTURAS DE CAPA DE DIRECTORIO*/
 struct entrada {
-    char nombre[60];  //En el SF ext2 la longitud del nombre es 255
+    char nombre[60];
     unsigned int ninodo;
 };
