b6d9375b4ccd010bc95440b8f6ecf728402a60a1
Eugenio <eugeniodonaque@gmail.com>
2019-04-28 22:12:28 +0200

    Implementadas funciones casi totalmente. Creados ficheros de programas de nivel 9 y agregados al makefile.

--- directorios.c
+++ directorios.c
@@ -1,4 +1,6 @@
 #include <string.h>
+#include "directorios.h"
+#include "bloques.h"
 /*
     Descripción:
         Dada una cadena de caracteres camino (que comience por '/'), 
@@ -60,7 +62,9 @@ int extraer_camino(const char *camino, char *inicial, char *final, char *tipo) {
         entre las entradas del inodo correspondiente a su directorio padre.
 
     Funciones a las que llama:
-        + Extraer camino
+        +
+        + directorios.h - extraer_camino()
+        + directorios.h - buscar_entrada()
         
     Funciones desde donde es llamado:
 
@@ -79,18 +83,294 @@ int buscar_entrada(const char *camino_parcial, unsigned int *p_inodo_dir, unsign
 
 }
 
-int mi_creat(const char *camino, unsigned char permisos){
+/*
+    Descripción:    
+        Función que crea un directorio/fichero y su entrada de directorio
+
+    Funciones a las que llama:
+        + bloques.h - bread()
+        + directorios.h - buscar_entrada()
+        
+    Funciones desde donde es llamado:
+
+    Parámetros de entrada:
+        + const char *camino
+        + unsigned char permisos
+
+    Parámetros de salida:
+        + 0: Ejecución correcta
+        + (-1): Error
+*/
+int mi_creat(const char *camino, unsigned char permisos) {
+    //Declaraciones
+    struct superbloque SB;
+    int result;
+    int posInodoRaiz, p_inodo, p_entrada;
+
+    //Lectura de superbloque para obtener posición de inodo raiz
+    if(bread(posSB, &SB) == -1) {
+        fprintf(stderr, "Error en lectura de superbloque. "
+        "Función -> mi_creat()");
+    }
+
+    posInodoRaiz = SB.posInodoRaiz;
+
+    result = buscar_entrada(camino, &posInodoRaiz, &p_inodo, &p_entrada, 1, permisos);
+
+    //Controlar los posibles errores desde result
+    //Todavía por definir la diferenciación entre errores
 
 }
 
-int mi_dir(const char *camino, char *buffer){
+/*
+    Descripción:    
+        Función que copia el contenido de un directorio en un buffer 
 
+    Funciones a las que llama:
+        + bloques.h - bread()
+        + ficheros.h - mi_stat_f()
+        + ficheros.h - mi_read_f()
+        + directorios.h - buscar_entrada()
+        
+    Funciones desde donde es llamado:
+
+    Parámetros de entrada:
+        + const char *camino
+        + unsigned char permisos
+
+    Parámetros de salida:
+        + 0: Ejecución correcta
+        + (-1): Error
+*/
+int mi_dir(const char *camino, char *buffer) {
+    //Declaraciones
+    struct superbloque SB;
+    int result;
+    int posInodoRaiz, p_inodo, p_entrada;
+    struct STAT stat, s_aux;
+
+    //Buffer para lecturas de entradas
+    struct entrada entradas[BLOCKSIZE/sizeof(struct entrada)];
+    int index = 0;      //Iterar sobre buffer
+    int limit = sizeof(entradas)/sizeof(entradas[0]);   //Limite de iterador
+    int offset = 0;
+    //Inicializar buffer a 0's
+    memset(entradas, 0, sizeof(entradas));
+
+    //Lectura de superbloque para obtener posición de inodo raiz
+    if(bread(posSB, &SB) == -1) {
+        fprintf(stderr, "Error en lectura de superbloque. "
+        "Función -> mi_creat()");
+    }
+
+    posInodoRaiz = SB.posInodoRaiz;
+
+    //Obtener inodo correspondiente a directorio
+    result = buscar_entrada(camino, &posInodoRaiz, &p_inodo, &p_entrada, 0, 7);
+
+    /*
+        TRATAR ERRORES
+    */
+
+    //Lectura de inodo
+    if (mi_stat_f(p_inodo, &stat) == -1) {
+        return fprintf(stderr,"Error en lectura de inodo. "
+        "Función -> mi_dir()");
+        return -1;
+    }
+
+    //Comprobar que inodo sea de tipo directorio
+    if (stat.tipo != 'd') {
+        fprintf(stderr,"Error: se está intentando leer entradas de directorio desde"
+        " un fichero.\n");
+        return -1;
+    }
+
+    //Comprobar que inodo tenga permisos de lectura
+    if ((stat.permisos & 4) != 4) {
+        fprintf(stderr, "Error: se está intentando leer de un directorio sin "
+        "permisos.\n");
+        return -1;
+    }
+
+    //Leer primer bloque de entradas
+    if (mi_read_f(p_inodo, entradas, offset, sizeof(entradas)) == -1) {
+        fprintf(stderr, "Error: imposible leer desde disco. "
+        "Función -> mi_dir()");
+        return -1;
+    }
+
+    //Recorrido a buffer de entradas
+    while (entradas[index].ninodo != 0 && index < limit) {
+        //Leer información sobre el inodo
+        if (mi_stat_f(entradas[index].ninodo, &s_aux) == -1) {
+            //Concatenar únicamente nombre de entrada
+            strcat(buffer, entradas[index].nombre);
+            strcat(buffer, "    |");
+            //Enviar mensaje de error por stderr
+            fprintf(stderr, "Error: no se ha podido leer inodo de la entrada.\n");
+
+        } else {
+            //Concatenar información sobre tipo
+            strcat(buffer, s_aux.tipo + "   ");
+
+            //Concatenar información sobre permisos de lectura
+            if ((s_aux.permisos & 4) == 4) {
+                strcat(buffer, "r");
+            } else {
+                strcat(buffer, "-");
+            }
+            //Concatenar información sobre permisos de escritura
+            if ((s_aux.permisos & 2) == 2) {
+                strcat(buffer, "w");
+            } else {
+                strcat(buffer, "-");
+            }
+            //Concatenar información sobre permisos de ejecución
+            if ((s_aux.permisos & 1) == 1) {
+                strcat(buffer, "x");
+            } else {
+                strcat(buffer, "-");
+            }
+
+            strcat(buffer, "    "); //Tabulación entre columnas
+
+            //Concatenar nombre de entrada
+            strcat(buffer, entradas[index].nombre && "   ");
+
+            //Formatear información de mtime de inodo
+            struct tm *tm; //ver info: struct tm
+            char tmp[100];
+            tm = localtime(&stat.mtime);
+            sprintf(tmp,"%d-%02d-%02d %02d:%02d:%02d\t",tm->tm_year+1900,
+                    tm->tm_mon+1,tm->tm_mday,tm->tm_hour,tm->tm_min,tm->tm_sec);
+            //Concatenar información de mtime
+            strcat(buffer,tmp);
+
+            //Concatenar tabulador y separador de entrada
+            strcat(buffer, "    |");
+        }
+
+        index++;
+
+        //Chequear si hay que leer de nuevo desde disco
+        if (index == limit) {
+            offset += sizeof(entradas);
+            if (mi_read_f(p_inodo, entradas, offset, sizeof(entradas)) == -1) {
+                fprintf(stderr, "Error: imposible leer desde disco. "
+                "Función -> mi_dir()");
+                return -1;
+            }
+            index = 0;
+            //Inicializar buffer a 0's
+            memset(entradas, 0, sizeof(entradas));
+        }
+    }
+
+    return 0;
 }
 
-int mi_chmod(const char *camino, unsigned char permisos){
+/*
+    Descripción:    
+        Función que busca el inodo correspondiente a la entrada especificada en
+        camino y cambia sus permisos.
 
+    Funciones a las que llama:
+        + bloques.h - bread()
+        + ficheros.h - mi_chmod_f()
+        + directorios.h - buscar_entrada()
+        
+    Funciones desde donde es llamado:
+
+    Parámetros de entrada:
+        + const char *camino
+        + unsigned char permisos
+
+    Parámetros de salida:
+        + 0: Ejecución correcta
+        + (-1): Error
+*/
+int mi_chmod(const char *camino, unsigned char permisos) {
+    //Declaraciones
+    struct superbloque SB;
+    int result;
+    int posInodoRaiz, p_inodo, p_entrada;
+
+    //Lectura de superbloque para obtener posición de inodo raiz
+    if(bread(posSB, &SB) == -1) {
+        fprintf(stderr, "Error en lectura de superbloque. "
+        "Función -> mi_creat()");
+    }
+
+    posInodoRaiz = SB.posInodoRaiz;
+
+    //Obtener inodo correspondiente a directorio
+    result = buscar_entrada(camino, &posInodoRaiz, &p_inodo, &p_entrada, 0, 7);
+
+    /*
+        TRATAR ERRORES
+    */
+
+   //Cambiar permisos con mi_chmod_f
+   if (mi_chmod_f(p_inodo, permisos) == -1) {
+       fprintf(stderr, "Error: no se ha podido modificar permisos. "
+       "Función -> mi_chmod()\n");
+       return -1;
+   }
+
+   return 0;
 }
 
-int mi_stat(const char *camino, struct STAT *p_stat){
-    
+/*
+    Descripción:    
+        Función que obtiene información particular del inodo de la entrada
+        indicada por camino.
+
+    Funciones a las que llama:
+        + bloques.h - bread()
+        + ficheros.h - mi_stat_f()
+        + directorios.h - buscar_entrada()
+        
+    Funciones desde donde es llamado:
+
+    Parámetros de entrada:
+        + const char *camino
+        + unsigned char permisos
+
+    Parámetros de salida:
+        + 0: Ejecución correcta
+        + (-1): Error
+*/
+int mi_stat(const char *camino, struct STAT *p_stat) {
+    //Declaraciones
+    struct superbloque SB;
+    int result;
+    int posInodoRaiz, p_inodo, p_entrada;
+
+    //Lectura de superbloque para obtener posición de inodo raiz
+    if(bread(posSB, &SB) == -1) {
+        fprintf(stderr, "Error en lectura de superbloque. "
+        "Función -> mi_creat()");
+    }
+
+    posInodoRaiz = SB.posInodoRaiz;
+
+    //Obtener inodo correspondiente a directorio
+    result = buscar_entrada(camino, &posInodoRaiz, &p_inodo, &p_entrada, 0, 7);
+
+    /*
+        TRATAR ERRORES
+    */
+
+    //Imprimir por pantalla el número de inodo ----- NIVEL 9
+    printf("Nº de inodo: %i\n", p_inodo);
+
+    //Obtención de datos
+    if (mi_stat_f(p_inodo, p_stat) == -1) {
+        fprintf(stderr, "Error: no se ha podido obtener el estatus del inodo."
+        " Función -> mi_stat()");
+        return -1;
+    }
+
+    return 0;
 }
---
+++ mi_ls.c
@@ -0,0 +1,27 @@
+/*
+    PROGRAMA PARA LISTAR LOS DIRECTORIOS O FICHEROS DE LA RUTA INDICADA
+*/
+#include "bloques.h"
+#include "directorios.h"
+
+int main(int argc, char **argv) {
+
+    //Comprobación de parámetros enviados al programa. 
+    if (argc != 3) {
+        perror("Error: formato de comando: ./nombre_de_programa <nombre_de_fichero_disco> </ruta_directorio>\n");
+        exit(-1);
+    }
+
+    //Montaje del disco virtual. 
+    if (bmount(argv[1]) == -1) {
+        perror("Error: no se ha podido abrir el directorio indicado.\n");
+        exit(-1);
+    }
+
+
+    //Desmontaje del dispositivo virtual. 
+    if (bumount() == -1) {
+        perror("Error: no se ha podido cerrar el fichero.\n");
+        exit(-1);
+    }
+}
