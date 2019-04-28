ea95fe3b70ccca14dc6ebb4444a5d4fe26f3cf7a
Eugenio <eugeniodonaque@gmail.com>
2019-04-28 18:24:18 +0200

    He completado las partes que faltaban del algoritmo buscar_entrada. Hice pequeñas modificaciones en extraer_camino. Por discutir en grupo

--- directorios.h
+++ directorios.h
@@ -7,8 +7,7 @@ int extraer_camino(const char *camino, char *inicial, char *final, char *tipo);
 int buscar_entrada(const char *camino_parcial, unsigned int *p_inodo_dir, unsigned int *p_inodo, unsigned int *p_entrada, char reservar, unsigned char permisos);
 
 //Estructura entrada. 
-
 struct entrada {
-char nombre[60];  //En el SF ext2 la longitud del nombre es 255
-unsigned int ninodo;
+    char nombre[60];  //En el SF ext2 la longitud del nombre es 255
+    unsigned int ninodo;
 };
