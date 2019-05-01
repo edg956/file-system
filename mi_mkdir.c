36d128780bd9f91a60e516141342b23bf160c7a1
Eugenio Doñaque <eugeniodonaque@gmail.com>
2019-05-01 16:14:46 +0200

    Eliminados archivos de nivel 7

--- mi_mkdir.c
+++ mi_mkdir.c
@@ -3,12 +3,21 @@
 */
 #include "bloques.h"
 #include "directorios.h"
+#include <string.h>;
 
 int main(int argc, char **argv) {
+
+    int result = 0;
 
     //Comprobación de parámetros enviados al programa. 
     if (argc != 4) {
         perror("Error: formato de comando: ./nombre_de_programa <nombre_de_fichero_disco> <permisos> </ruta>\n");
+        exit(-1);
+    }
+
+    //Comprobar correción de permisos introducidos
+    if (strlen(argv[2]) != 1 || (atoi(argv[2][0]) & 248) != 0) {
+        perror("Error: rango permitido para permisos: [0,7]");
         exit(-1);
     }
 
@@ -18,6 +27,13 @@ int main(int argc, char **argv) {
         exit(-1);
     }
 
+    //Llamada a mi_creat para crear directorio/fichero
+    result = mi_creat(argv[3], argv[2][0]);
+
+    if (result == -1) {
+        perror("Error: no se ha podido crear directorio o fichero.");
+        exit(-1);
+    }
 
     //Desmontaje del dispositivo virtual. 
     if (bumount() == -1) {
