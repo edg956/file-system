46eabf45ebc85b225e2db2f43fbf1d0bf9135bf4
Alvaro Pons <varopons@gmail.com>
2019-05-02 16:17:37 +0000

    cabeceras nivel 10 añadidas

--- directorios.h
+++ directorios.h
@@ -13,6 +13,9 @@ int mi_stat(const char *camino, struct STAT *p_stat);
 int mi_touch(const char *camino, unsigned char permisos);
 
 int control_errores_buscar_entrada(int nerror, char *buffer);
+int mi_read (const char *camino, void *buf, unsigned int offset, unsigned int nbytes);
+int mi_write (const char *camino, const void *buf, unsigned int offset, unsigned int nbytes);
+
 
 /*ESTRUCTURAS DE CAPA DE DIRECTORIO*/
 struct entrada {
