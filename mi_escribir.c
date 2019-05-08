feebd914943fd441b15fff985dd608c015b4ea29
Alvaro Pons <varopons@gmail.com>
2019-05-08 18:28:45 +0000

    mi_escribir.c falta probar

--- mi_escribir.c
+++ mi_escribir.c
@@ -11,4 +11,33 @@ Ha de mostrar la cantidad de bytes escritos.
 */
 int main (int argc, char **argv) {
 
+int longitud = 0;
+int offset = 0;
+int bytesEscritos = 0;
+
+if(argc != 5){
+    perror("Error de sintaxis: mi_escribir <disco> </ruta_fichero> <texto> <offset>\n");
+    exit(-1);
+}
+char *camino = argv[2];
+
+if(camino[strlen(camino) - 1] == '/'){
+    perror("Error: La ruta no es un fichero.\n");
+    exit(-1);
+}
+
+offset = atoi(argv[4]); // no se si esta bien pero sino no sabia como hacerlo
+longitud = strlen(argv[3]);
+char buffer[longitud];
+strcpy(buffer, argv[3]);
+
+bytesEscritos = mi_write(camino, &buffer, offset, longitud);
+if(bytesEscritos == -1){
+    perror ("Error.\n");
+    exit(-1);
+}
+
+printf("Longitud texto: %i\n", longitud);
+printf("Bytes escritos: %i\n", bytesEscritos);
+
 }
