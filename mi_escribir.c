9287e03868058ab30c62c2467f22884ced4a5cda
Alvaro Pons <varopons@gmail.com>
2019-05-08 17:22:16 +0000

    New file mi_escribir.c

---
+++ mi_escribir.c
@@ -0,0 +1,14 @@
+#include "directorios.h"
+
+
+/*
+Permite escribir texto en una posición de un fichero (offset).
+    - Ha de recibir comoo parámetro la ruta del fichero en vez del ninodo.
+    - Eliminar el argumento de diferentes_inodos y añadir el de offset para poder
+    indicar desde consola que vamos a escribir.
+Hay que comprobar que se trate de un fichero, y que tenga permisos de escritura.
+Ha de mostrar la cantidad de bytes escritos.
+*/
+int main (int argc, char **argv) {
+
+}
