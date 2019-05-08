b14546c9cc62c06c925bcd8f7212cd93f1e67f33
Alvaro Pons <varopons@gmail.com>
2019-05-08 16:24:22 +0000

    cabecera mi_cat.c

---
+++ mi_cat.c
@@ -0,0 +1,14 @@
+#include 'directorios.h'
+
+/*
+Programa (comando) que muestra TODO el contenido de un fichero.
+    - Comprovar que la ruta se corresponde con la de un fichero y no a la de un directorio
+    - Han de coincidir los bytes leidos con el tamaño en bytes lógicos del fichero y con el tamaño físico
+    del fichero externo al que redireccionemos la lectura, y se ha de filtrar la basura
+    - Utilizar una variable tambuffer para poder cambiar el tamaño del buffer de lectura sin tener que modificar 
+    todas las sentencias involucradas.
+*/
+int main (int argc, char **argv){
+    
+    
+}
