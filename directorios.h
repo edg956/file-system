d48d4b1fb1ccc120b913b4b2181a7791cf0dbde2
Nadal12 <nadal12@hotmail.com>
2019-05-09 19:41:21 +0200

    Agregado mi_link a directorios.c. Añadido programa mi_link.c

--- directorios.h
+++ directorios.h
@@ -12,7 +12,7 @@ int mi_chmod(const char *camino, unsigned char permisos);
 int mi_stat(const char *camino, struct STAT *p_stat);
 int mi_read (const char *camino, void *buf, unsigned int offset, unsigned int nbytes);
 int mi_write (const char *camino, const void *buf, unsigned int offset, unsigned int nbytes);
-
+int mi_link(const char *camino1, const char *camino2);
 
 /*ESTRUCTURAS DE CAPA DE DIRECTORIO*/
 struct entrada {
