fdb02712afc974bad3082bacfc9afea175f7078d
nadal <nadal12@hotmail.com>
2019-05-10 09:45:25 +0200

    Se continua con el rebase

--- directorios.h
+++ directorios.h
@@ -10,15 +10,15 @@ int mi_creat(const char *camino, unsigned char permisos);
 int mi_dir(const char *camino, char *buffer);
 int mi_chmod(const char *camino, unsigned char permisos);
 int mi_stat(const char *camino, struct STAT *p_stat);
-<<<<<<< HEAD
 int mi_touch(const char *camino, unsigned char permisos);
 
 int control_errores_buscar_entrada(int nerror, char *buffer);
-=======
+
 int mi_read (const char *camino, void *buf, unsigned int offset, unsigned int nbytes);
 int mi_write (const char *camino, const void *buf, unsigned int offset, unsigned int nbytes);
 
->>>>>>> cabeceras nivel 10 añadidas
+int mi_read (const char *camino, void *buf, unsigned int offset, unsigned int nbytes);
+int mi_write (const char *camino, const void *buf, unsigned int offset, unsigned int nbytes);
 
 /*ESTRUCTURAS DE CAPA DE DIRECTORIO*/
 struct entrada {
