f2e862a4c9d3c581210ab3f6f90afa41ed05a6fe
Eugenio <eugeniodonaque@gmail.com>
2019-05-20 14:55:33 +0200

    Creado programa y script para realizar pruebas

---
+++ leer.c
@@ -0,0 +1,46 @@
+/*
+    PROGRAMA DE APOYO PARA DETERMINAR LA CORRECIÓN DE LA INFORMACIÓN
+    ALMACENADA EN EL SUPERBLOQUE, EN EL MAPA DE BITS Y EN EL ARRAY DE INODOS
+*/
+#include <time.h>
+#include "bloques.h"
+#include "ficheros_basico.h"
+
+#define NUM_ATTR_SB 6 //Número de atributos sobre los cuales iterar para 
+                      //función leer_bit()
+#define POS_INODO_RAIZ 0 //Posición del inodo raiz dentro del array de inodos
+
+int main(int argc, char **argv) {
+
+    //Comprobación de parámetros enviados al programa. 
+    if (argc != 2) {
+        perror("Error: formato de comando: ./nombre_de_programa <nombre_de_fichero_disco>\n");
+        exit(-1);
+    }
+
+    //Montaje del disco virtual. 
+    if (bmount(argv[1]) == -1) {
+        perror("Error: no se ha podido abrir el directorio indicado.\n");
+        exit(-1);
+    }
+
+    //Estructuras y variables de apoyo.
+    struct superbloque SB;
+
+    //Lectura del superbloque.
+    if (bread(posSB, &SB) == -1) {
+        perror("Error: no se ha podido obtener información del superbloque del FS\n");
+        exit(-1);
+    }
+
+    /**************************DATOS DEL SUPERBLOQUE***************************/
+    puts("DATOS DEL SUPERBLOQUE\n");
+    printf("Cantidad de bloques libres: %i\n",SB.cantBloquesLibres);
+    printf("Cantidad de inodos libres: %i\n",SB.cantInodosLibres);
+
+    //Desmontaje del dispositivo virtual. 
+    if (bumount() == -1) {
+        perror("Error: desmontar dispositivo erroneo.");
+        exit(-1);
+    }
+}
